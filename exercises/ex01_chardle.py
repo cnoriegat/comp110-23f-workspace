b08bd09d17b63895a7b92d0745469932081a6f6e
Camila Noriega <cnoriega@unc.edu>
2023-09-08 10:32:19 -0400

    Ex01 - Try 2

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,47 +2,47 @@
 
 __author__ = "730621572"
 
-#prompting word
+# prompting word
 word_insert: str = input("Enter a 5-character word: ")
 if len(word_insert) != 5:
     print("Error: Word must contain 5 characters")
     exit()
 
-#prompting character
+# prompting character
 character_insert: str = input("Enter a single character: ")
 if len(character_insert) != 1:
     print("Error: Character must be a single character.")
     exit()
 
-#character match count
+# character match count
 num_matches: int = 0
 
-#early exit for invalid inputs
+# early exit for invalid inputs
 if len(word_insert) != 5:
     print("Error: Word must contain 5 characters")
     exit()
 
 print("Searching for " + character_insert + " in " + word_insert)
 
-#checking indices for matches
+# checking indices for matches
 if character_insert == (word_insert[0]):
-    print(character_insert + " found at index 1")
+    print(character_insert + " found at index 0")
     num_matches = num_matches + 1
 
 if character_insert == (word_insert[1]):
-    print(character_insert + " found at index 2")
+    print(character_insert + " found at index 1")
     num_matches = num_matches + 1
 
 if character_insert == (word_insert[2]):
-    print(character_insert + " found at index 3")
+    print(character_insert + " found at index 2")
     num_matches = num_matches + 1
 
 if character_insert == (word_insert[3]):
-    print(character_insert + " found at index 4")
+    print(character_insert + " found at index 3")
     num_matches = num_matches + 1
 
 if character_insert == (word_insert[4]):
-    print(character_insert + " found at index 5")
+    print(character_insert + " found at index 4")
     num_matches = num_matches + 1
 
 # matching indices
