a56760ee4e556e8b4c0659aa35bca542410f9824
Camila Noriega <cnoriega@unc.edu>
2023-09-20 18:31:34 -0400

    Finalprogressex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -16,10 +16,11 @@ result: str = ""
 while len(word_guess) != len(secret_word):
     word_guess = input(f"That was not {len(secret_word)} letters! Try again: ")
 
-# emoji output (green, yellow, white)
+# checking indices for correct matches
 while idx < len(secret_word):
     if word_guess[idx] == secret_word[idx]:
         result += green_box
+    # checking indices for correct letters at incorrect positions
     else:
         chr_found: bool = False 
         alt_idx: int = 0
@@ -29,13 +30,15 @@ while idx < len(secret_word):
             alt_idx += 1
         if chr_found is True:
             result += yellow_box 
+        # checking indices for incorrect letters
         else:
             result += white_box  
     idx += 1     
 print(result)
 
-# game ending - correct or incorrect answers
+# correct guess
 if word_guess == secret_word:
     print("Woo! You got it!")
+# incorrect guess
 elif len(word_guess) == len(secret_word):
     print("Not quite. Play again soon!")
