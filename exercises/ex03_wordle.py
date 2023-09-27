f47271809fb7c03fb9324a5bdc353843888efc06
Camila Noriega <cnoriega@unc.edu>
2023-09-27 12:14:47 -0400

    Progress

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,8 +1,10 @@
 """Structured Wordle."""
 
+
 __author__ = "730621572"
 
-def contains_char (word_input: str, char: str) -> bool:
+
+def contains_char(word_input: str, char: str) -> bool:
     """Check if word input contains character."""
     assert len(char) == 1
     idx: int = 0
@@ -13,7 +15,8 @@ def contains_char (word_input: str, char: str) -> bool:
         idx += 1
     return result
 
-def emojified (guess: str, secret: str) -> str:
+
+def emojified(guess: str, secret: str) -> str:
     """Print emoji string based on guess."""
     assert len(guess) == len(secret)
     result: str = ""
@@ -31,28 +34,32 @@ def emojified (guess: str, secret: str) -> str:
         idx += 1
     return result
 
+
 def input_guess(exp_len: int) -> str:
     """Prompt users until correct length."""
     word_guess: str = input(f"Enter a {exp_len} character word: ")
     while len(word_guess) != exp_len:
-        word_guess = input(f"That was not {exp_len} chars! Try again: ")
+        word_guess = input(f"That wasn't {exp_len} chars! Try again: ")
     return word_guess
+
 
 def main() -> None:
     """The entrypoint of the program and main game loop."""
     secret_word: str = "codes"
     num_turns: int = 1
+    max_turns: int = 6
     win: bool = False
-    while num_turns <= len(secret_word) and not win:
-        print(f"=== Turn {num_turns}/{len(secret_word)} ===")
+    while num_turns <= max_turns and not win:
+        print(f"=== Turn {num_turns}/{max_turns} ===")
         word_guess: str = input_guess(len(secret_word))
         print(emojified(word_guess, secret_word))
         if secret_word == word_guess:
-            print(f"You won in {num_turns}/{len(secret_word)} turns!")
+            print(f"You won in {num_turns}/{max_turns} turns!")
             win = True
         num_turns += 1
     if not win:
-        print(f"X/{num_turns} - Sorry, try again tomorrow!")
+        print(f"X/{max_turns} - Sorry, try again tomorrow!")
+
 
 if __name__ == "__main__":
     main()
